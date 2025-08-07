bdd9713ba8d138f2a7a11d8775971d6d24f7550d
Creez32 <cristiannicolas32@hotmail.com>
2025-08-07 12:41:17 -0300

    Se trabajo en react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import Home from './routes/Home/Home'
-import Header from './components/Header/Header'
-import Login from './routes/Login/Login'
+import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+//Components
+import Header from './components/Header/Header';
+import Home from './routes/Home/Home';
+import Login from './routes/Login/Login';
+import Historial from './routes/Historial/Historial';
+import Plantillas from './routes/Plantillas/Plantillas';
+import Correos from './routes/Correos/Correos';
 
 function App() {
 
     return (
         <>
-            <Header />
-            <Home />
-            {/* <Login /> */}
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={
+                        <Home />
+                    } />
+                    <Route path="/Historial" element={
+                        <Historial />
+                    } />
+                    <Route path="/Correos" element={
+                        <Correos />
+                    } />
+                    <Route path="/Plantillas" element={
+                        <Plantillas />
+                    } />
+                    <Route path="/Login" element={
+                        <Login />
+                    } />
+                </Routes>
+            </BrowserRouter>
         </>
     )
 }
